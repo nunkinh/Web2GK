775c7e46460ff6588a79b0fbf17ddc1a8bb3f18f
nunkinh <nunkinh@gmail.com>
2017-04-22 00:34:45 +0700

    the first time upload

---
+++ Web2/Web2/Models/BUS/SanPhamBus.cs
@@ -0,0 +1,17 @@
+﻿using PhoneConnection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web2.Models.BUS
+{
+    public class SanPhamBus
+    {
+        public static List<SanPham> DanhSach()
+        {
+            var db = new PhoneConnectionDB();
+            return db.Query<SanPham>("select * from SanPham").ToList<SanPham>();
+        }
+    }
+}
